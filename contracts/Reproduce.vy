0ca5acc4157d2fed852b149eabb7fb4bcd914363
CryptoDegen <degencryptomonkey@gmail.com>
2022-04-27 10:38:47 +0200

    Bugfix

--- contracts/Reproduce.vy
+++ contracts/Reproduce.vy
@@ -1,8 +1,10 @@
 # @version 0.3.1
 
+amount: public(uint256)
+
 @external
 def deposit(_amount: uint256 = MAX_UINT256):
-        return
+        self.amount = _amount
 
 @external
 def withdraw() -> uint256:
--- scripts/reproduce.js
+++ scripts/reproduce.js
@@ -1,15 +1,21 @@
-const { ethers } = require('hardhat');
+const { ethers } = require("hardhat");
 
 async function main() {
-  const Reproduce = await ethers.getContractFactory('Reproduce');
+  const Reproduce = await ethers.getContractFactory("Reproduce");
   const reproduceContract = await Reproduce.deploy();
   await reproduceContract.deployed();
-  
+
+  console.log(reproduceContract.functions);
+
   // Function `withdraw` is recognized because it doesn't use function overloading
-  console.log(reproduceContract.withdraw);
+  //console.log(reproduceContract.withdraw);
 
   // This line of code will throw an error because function `deposit` is undefined!
-  console.log(await reproduceContract.deposit(10));
+  console.log(await reproduceContract["deposit(uint256)"](10));
+  //console.log(await reproduceContract.deposit(10));
+
+  const currentDeposit = await reproduceContract.amount();
+  console.log("Deposit amount: ", currentDeposit);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
